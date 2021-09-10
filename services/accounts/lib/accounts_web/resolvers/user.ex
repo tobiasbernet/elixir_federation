bf6429608bf3cc6cfe9ce11a753c5dc00b88a7ec
Tobias Bernet <tobias.bernet@shopviu.com>
2021-09-10 10:01:07 +0200

    Workaround for resolve_reference

--- services/accounts/lib/accounts_web/resolvers/user.ex
+++ services/accounts/lib/accounts_web/resolvers/user.ex
@@ -1,5 +1,19 @@
 defmodule AccountsWeb.Resolvers.User do
-  use ExMachina
+  @doc """
+  The return value of the `_resolve_reference` needs to be a struct or `%{__typename: typename}`.
+
+  See: https://github.com/DivvyPayHQ/absinthe_federation/blob/main/lib/absinthe/federation/schema/entity_union.ex#L24
+  """
+  defstruct [:id, :name, :birthDate, :username]
+
+  def find(_parent, %{__typename: "User", id: id}, _resolution) do
+    result =
+      users()
+      |> Enum.find(fn u -> u.id == id end)
+      |> IO.inspect()
+
+    {:ok, result}
+  end
 
   def find(_parent, _arg, _resolution) do
     {:ok, List.first(users())}
@@ -7,13 +21,13 @@ defmodule AccountsWeb.Resolvers.User do
 
   defp users do
     [
-      %{
+      %__MODULE__{
         id: "1",
         name: "Ada Lovelace",
         birthDate: "1815-12-10",
         username: "@ada"
       },
-      %{
+      %__MODULE__{
         id: "2",
         name: "Alan Turing",
         birthDate: "1912-06-23",
--- services/accounts/lib/accounts_web/schema/user_types.ex
+++ services/accounts/lib/accounts_web/schema/user_types.ex
@@ -2,11 +2,23 @@ defmodule AccountsWeb.Schema.UserTypes do
   use Absinthe.Schema.Notation
   use Absinthe.Federation.Notation
 
+  alias AccountsWeb.Resolvers
+
   object :user do
     key_fields("id")
 
     field(:id, non_null(:id))
     field(:name, :string)
     field(:username, :string)
+
+    field(:_resolve_reference, :user) do
+      # resolve(&Resolvers.User.find/3)
+      # raises ->
+      # (FunctionClauseError) no function clause matching in Absinthe.Middleware.unshim/2
+      #   (absinthe 1.6.5) lib/absinthe/middleware.ex:293: Absinthe.Middleware.unshim([{Absinthe.Middleware.Telemetry, []}, {{Absinthe.Resolution, :call}, &AccountsWeb.Resolvers.User.find/3}], AccountsWeb.Schema)
+
+      # So we need to call the resolver in a anonymous function:
+      resolve(fn _, args, _ -> Resolvers.User.find(nil, args, nil) end)
+    end
   end
 end
